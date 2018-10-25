46edf71515c2ede9f54cb4cd28e15ca7f5a60f57
mattyplo <mattploszaj@gmail.com>
2018-10-24 18:40:43 -0700

    got login to work.  Authorize is also in effect.  Logout link has been posted in side navigation bar under users view

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -18,6 +18,31 @@ class UsersController extends AppController
      *
      * @return \Cake\Http\Response|void
      */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Auth->allow(['logout']);
+    }
+
+    public function logout()
+    {
+        $this->Flash->success('You are now logged out.');
+        return $this->redirect($this->Auth->logout());
+    }
+    
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            } else {
+                $this->Flash->error(__('Username or password is incorrect'));
+            }
+        }
+    }
+    
     public function index()
     {
         $users = $this->paginate($this->Users);
