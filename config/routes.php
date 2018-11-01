eb2753e8233dd2db555a1b0d7b3380504313d485
mattyplo <mattploszaj@gmail.com>
2018-10-31 23:07:10 -0700

    back on track

--- config/routes.php
+++ config/routes.php
@@ -9,12 +9,17 @@ use Cake\Routing\Route\DashedRoute;
 Router::defaultRouteClass(DashedRoute::class);
 
 Router::scope('/', function (RouteBuilder $routes) {
-
-    $routes->setExtensions(['json']);
-    $routes->resources('Recipes');
+    
+    /*  This set's the Users controller and view as the index page!
+    $routes->connect('/', ['controller' => 'Users', 'action' => 'index']);
+    */
+    
     $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
   
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
     $routes->fallbacks(DashedRoute::class);
 });
+
+
+//https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GOOG&apikey=L1G9JSZ77QFGSV8A
